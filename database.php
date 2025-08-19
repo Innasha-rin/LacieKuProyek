f1953fca3a58291b2fc39bb8ff266ce53230041d
Innasha-rin <myusuframadhan758@gmail.com>
2025-08-19 17:55:03 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "sgp.domcloud.co";
-$dbname = "project_ollie_db";
-$username = "project-ollie";  // Sesuaikan dengan username database
-$password = "XhC86(Zh6)g67YTio(";  // Sesuaikan dengan password database
+$dbname = "radja_proyek_perpustakaan_db";
+$username = "radja-proyek-perpustakaan";  // Sesuaikan dengan username database
+$password = "r3u2b)w)XAi_P47AW1";  // Sesuaikan dengan password database
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
